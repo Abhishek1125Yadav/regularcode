f65a979acc084361870a71e14e28121cade72fc1
AbhishekYadav <abhiy5215@gmail.com>
2025-04-18 23:33:57 +0530

    Here is Code to Find Total Number Of Odd and Even Element present in array

---
+++ AllJavaCode/src/Allcode/TotalNumOddElement.java
@@ -0,0 +1,39 @@
+package Allcode;
+
+import java.util.*;
+public class TotalNumOddElement {
+
+	public static void main(String x[]) {
+		
+		Scanner data = new Scanner(System.in);
+		
+		System.out.println("Enter The Size");
+		int size = data.nextInt();
+		
+		int a[] = new int[size];
+		
+		System.out.println("Enter " + size + " Element Of An Array");
+		
+		for(int i = 0 ; i <= a.length - 1; i++) {
+			
+			a[i] = data.nextInt();
+			
+		}
+
+		int count = 0;
+		
+		for(int i = 0 ; i < a.length ; i++) {
+			
+			if(a[i] % 2 == 1) {
+				
+				count++;
+				
+			}
+			
+		}
+		
+		System.out.println("Total Number Of Odd Element is :- " + count);
+		
+	}
+	
+}
